d4ddbae8821cd6061f91c0d5f5cd01a9aee0c096
Syed Bilal Ahmed <syedbilal@Syeds-MacBook-Pro.local>
2025-02-14 15:16:25 +0500

    updated vercel.json

--- Backend/Models/Product.model.js
+++ Backend/Models/Product.model.js
@@ -1,31 +1,3 @@
-// import mongoose from "mongoose";
-
-// const productSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true,
-//     },
-//     image: {
-//         type: String,
-//         required: true,
-//     },
-//     description: {
-//         type: String,
-//         required: true,
-//     },
-//     price: {
-//         type: Number,
-//         required: true,
-//     },
-//     rating: {
-//         type: Number,
-//         default: 0,
-//     }
-// });
-
-// export const Product = mongoose.model("Product", productSchema); 
-
-
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
