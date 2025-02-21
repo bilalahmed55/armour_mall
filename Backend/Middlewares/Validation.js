fee4a1753f1421ce62b0b0ab635e1348dc88a8a9
Syed Bilal Ahmed <syedbilal@Syeds-MacBook-Pro.local>
2025-02-21 12:54:20 +0500

    updated signup

--- Backend/Middlewares/Validation.js
+++ Backend/Middlewares/Validation.js
@@ -5,6 +5,7 @@ export const signupValidation = (req, res, next) => {
         name: Joi.string().min(3).max(20).required(),
         email: Joi.string().email().required(),
         password: Joi.string().min(6).required(),
+        role: Joi.string().valid('user', 'seller', 'admin').default('user')
     });
     const { error } = schema.validate(req.body);
     if (error) {
